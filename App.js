cbc5635170ef31d4171d467e6ae92e59799fdae6
MarkAlperin <mark.t.alperin@gmail.com>
2022-05-31 15:04:39 -0500

    first commit, made basic App.js with PanResponder Draggable logic

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  PanResponder,
+  Animated,
+  Dimensions,
+} from "react-native";
+
+import Card from "./components/Card";
+import Table from "./components/Table";
+import PlayerHand from "./components/PlayerHand";
 
 export default function App() {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value,
+        });
+      },
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }]),
+      onPanResponderRelease: () => {
+        console.log("panRelease!");
+        pan.flattenOffset();
+      },
+    })
+  ).current;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Table text="I am a card table" />
+      <PlayerHand text="I am a card table" />
+
+      <Animated.View
+        style={{ transform: [{ translateX: pan.x }, { translateY: pan.y }] }}
+        {...panResponder.panHandlers}
+      >
+        <Card text="I am a card" />
+      </Animated.View>
+      {/* <PlayerHand text="I am a player hand" /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +53,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-evenly",
   },
 });
